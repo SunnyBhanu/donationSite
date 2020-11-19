b98ed8b8ab20fe9ac1515872c0dcffb8a08ffec1
Sunny Bhanu <72149962+SunnyBhanu@users.noreply.github.com>
2020-11-20 00:59:12 +0530

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,26 @@
+<?php
+require('./instamojo.php');
+const API_KEY ="test_48cb11aba62b95c8b18a1473b97";
+const AUTH_TOKEN = "test_c0d4066740b70396c12f55f9835";
+
+
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
+{
+    $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
+    
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => 'Donation',
+            "buyer_name" => $_POST['name'],
+            "amount" => $_POST['amount'],
+            "send_email" => true,
+            "email" => $_POST['email'],
+            "redirect_url" => "http://paymentgatewaydonation.epizy.com/success.html"
+            ));
+        header('Location:'. $response['longurl']);
+    }
+    catch (Exception $e) {
+        print('Error: ' . $e->getMessage());
+    }
+}
+?>
